dd9cd9d76def3e5c88da7967f64ae81b91c10f8b
Nuwini <nuwi19949@gmail.com>
2016-10-03 13:48:22 +0530

    Initial commit

---
+++ view/pages/about_us.php
@@ -0,0 +1,287 @@
+<?php
+include('../../model/config.php');
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>UCSC</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<link rel="stylesheet" href="../layout/styles/layout.css" type="text/css" />
+<!-- 3 Column Stylesheet Added To The Page And Not To The Layout.css -->
+<link rel="stylesheet" href="../layout/styles/3_column.css" type="text/css" />
+</head>
+<body id="top">
+<div class="wrapper row1">
+  <div id="topnav">
+    <ul>
+      <li class="active"><a href="../../index.html"><strong>Home</strong></a></li>
+      <li><a href="3-columns.html"><strong>About Us</strong></a></li>
+      <li><a href="full-width1.html"><strong>Academic Programmes</strong></a></li>
+      <li><a href=""><strong>People</strong></a></li>
+      <li><a href="gallery.html"><strong>Gallery</strong></a></li>
+      <li><a href="infrastructure_and_services.html"><strong>Services</strong></a></li>
+      <li><a href="#"><strong>News/Notices</strong></a></li>
+      <li class="last"><a href="#"><strong>Contact Us</strong></a></li>
+    </ul>
+  </div>
+</div>
+<!-- ####################################################################################################### -->
+<div class="wrapper row2">
+  <div id="header" class="clear"><span class="img">
+    <h1><a href="index.html"><img src="../images/demo/logo-ucsc-1.png"/></a></h1>
+  </span>
+    <div class="img">
+      <h1>&nbsp;</h1>
+      <p></p>
+    </div>
+    <div class="fl_right">
+      <form action="#" method="post" id="sitesearch">
+        <fieldset>
+          <legend>Site Search</legend>
+          <input type="text" />
+          <input type="image" src="../images/demo/search.gif" id="search" alt="Search" />
+        </fieldset>
+      </form>
+      <p><a href="#">Site Map</a> | <a href="login_index.html">Login</a></p>
+    </div>
+  </div>
+</div>
+<!-- ####################################################################################################### -->
+<div class="wrapper row4">
+  <div id="quicknav" class="clear">
+    <ul>
+      <li><a href="#">Current Students</a>
+        <ul>
+          <li><a href="#">&raquo; Career Guidance Unit</a></li>
+          <li><a href="#">&raquo; Student Accommodation</a></li>
+          <li><a href="#">&raquo; Student Directory</a></li>
+          <li><a href="#">&raquo; Student Facilities</a></li>
+          <li class="last"><a href="#">&raquo; Students Union</a></li>
+        </ul>
+      </li>
+      <li><a href="#">Prospective Students</a>
+        <ul>
+          <li><a href="#">&raquo; University Life</a></li>
+          <li><a href="#">&raquo; Academic Courses</a></li>
+          <li><a href="#">&raquo; How To Apply</a></li>
+          <li><a href="#">&raquo; Cost And Fees</a></li>
+          <li class="last"><a href="#">&raquo; Visit The University</a></li>
+        </ul>
+      </li>
+      <li><a href="#">International Students</a>
+        <ul>
+          <li><a href="#">&raquo; Why Our University</a></li>
+          <li><a href="#">&raquo; How To Apply</a></li>
+          <li class="last"><a href="#">&raquo; Cost &amp; Finances</a></li>
+        </ul>
+      </li>
+      <li class="last"><a href="#">Academic Departments</a>
+        <ul>
+          <li><a href="#">&raquo; Department of communication and media technologies(CMT)</a></li>
+          <li><a href="#">&raquo; Department of Information Systems Engineering(ISE)</a></li>
+          <li class="last"><a href="#">&raquo; Department of communication and intelligent systems (CIS)</a></li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+</div>
+<!-- ####################################################################################################### -->
+<div class="wrapper row5">
+  <div id="container" class="clear"> 
+    <!-- ####################################################################################################### -->
+    <div id="left_column">
+      <div class="holder">
+        <h2 class="title">Quick Links</h2>
+        <ul class="nostart">
+          <li><a href="#">Alumni</a></li>
+          <li><a href="#">Events</a></li>
+          <li><a href="#">Job opportunities</a></li>
+          <li><a href="#">Maps and Directions</a></li>
+          <li><a href="#">Online Courses</a></li>
+          <li><a href="#">Postgraduate research</a></li>
+          <li><a href="#">Research</a></li>
+          <li><a href="undergraduate_studies.html">Undergraduate Courses</a></li>
+        </ul>
+      </div>
+      <div class="holder">
+        <h2 class="title">CONFERENCES AND SYMPOSIUMS</h2>
+        <div class="imgholder"><a href="conferences_and_sysmposiums.html"><img src="../images/demo/icter.png" alt="" /></a></div>
+        <div class="imgholder last"><a href="conferences_and_sysmposiums.html"><img src="../images/demo/symposium.png" alt="" /></a></div>
+      </div>
+    </div>
+    <!-- ############ -->
+    <div id="content">
+      <h1 class="title">University of Colombo School of Computing</h1>
+      <div class="imgholder"><img src="../images/demo/ucsc.png" alt="" /></div>
+      <p>The University of Colombo School of Computing (UCSC) is an integral part of the University of Colombo ,and has a history over 50 years as the leading computing educational in Sri Lanka.</p>
+
+<p>Computer education at University of Colombo was established way back in 1967 by the historic affiliation of the Statistical Consultancy and Data Processing Service Centre by the salient guidance and efforts of late Professor.V.K.Samaranayake ,who pioneered computing and IT development and usage in Sri Lanka and thus considered as the “Father if Information Technology” in Sri Lanka. He was a Professor of computer science and a former dean of the Faculty of Science ,University of Colombo. Professor Samaranayake was at the time of his death ,the chairman of the Information and Communication Technology Agency(ICTA) of Sri Lanka.</p>
+
+<p>The lead role of the Department of Statistics and Computer Science (DSCS) was to offer special degree programme in Computer Science. DSCS launched the first post-graduate programme in computing offered by a Sri Lankan university in 1986 and later in 1990 instantiated a special programme in computer science which subsequently produced a scant forty graduates per batch which was desperately insufficient to meet the increasing demand for professionals to various disciplines in the field of computer science. Considering the domain ICT was reigning and the golden opportunities which the institute already posed ,it became the right counterpart for the Department of Computer Science to achieve the goals ,the latter envisioned for. Thus ,happened the historic merge between the industry focused Institute of Computer Technology and the more theoretically oriented Department of Computer Science and gave birth to University of Colombo School of Computing(UCSC) on the 01st of Sseptember,2002.The UCSC can be now identified as an integral part of University of Colombo.</p>
+
+<p>At present ,UCSC recruits more than 1000 in housing students and 400 post graduate students. The students who are admitted to the UCSC may read for a degree programme in Computer Science(CS) OR Information Systems(IS).The UCSC offers three year and four year degree programmes in each of these disciplines.B.sc(Hons) in Computer Science or the degree in Bachelor of Science Honours in Software Engineering. Under the Postgraduate Masters programme of the UCSC ,students can read for the Master of Computer Science(MCS),Master of Information Technology(MIT) and Master of Science in Information Security(MIS).UCSC also offers Mphil and PhD research programmes.</p>
+
+	  <h2> <class="title">Academic Departments</h2>
+      <table summary="Summary Here" cellpadding="0" cellspacing="0">
+        <thead>
+          <tr>
+            <th>Department</th>
+            <th>Dpt. Head Name</th>
+            <th>Contact No.</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr class="light">
+            <td>Department of communication and media technologies(CMT)</td>
+            <td>Mr.G.K.A.Dias</td>
+            <td>(+94)112158955</td>
+            <td>gkad@ucsc.cmb.ac.lk</td>
+          </tr>
+          <tr class="dark">
+            <td>Department of Information Systems Engineering(ISE)</td>
+            <td>Prof.K.P.Hewagamage</td>
+            <td>(+94)112158945</td>
+            <td>kph@ucsc.cmb.ac.lk</td>
+          </tr>
+          <tr class="light">
+            <td>Department of communication and intelligent systems (CIS)</td>
+            <td>Dr.Ajantha Athukorale</td>
+            <td>(+94)112158985</td>
+            <td>aja@ucsc.cmb.ac.lk</td>
+          </tr>
+        </tbody>
+      </table>
+      <h2 class="title">Abbreviations</h2>
+      <ul>
+        <li>ADMTC – Advanced Digital Multimedia Technology Centre</li></br>
+        <li>CMT – Communication and Media Technologies</li></br>
+        <li>CSC – Computing Services Centre</li></br>
+        <li>CIS – Computation and Intelligent Systems(CIS)</li></br>
+        <li>DFC – Digital Forensic Centre</li></br>
+        <li>EDC – External Degrees Centre</li></br>
+        <li>eLC– E-Learning Centre</li></br>
+        <li>ISE – Information Systems Engineering</li></br>
+        <li>NOC – Network Operating Centre</li></br>
+        <li>PDC – Professional Development Centre</li></br>
+        <li>UCSC – University of Colombo School of Computing</li>
+      </ul>
+      </div>
+    <!-- ############ -->
+    <div id="right_column">
+      <div class="holder">
+        <h2 class="title">Goals</h2>
+        <ol>
+          <li>A centre of excellence in teaching and learning</li></br>
+          <li>Cross disciplinary research in computing</li></br>
+          <li>To create socially responsible professionals</li></br>
+          <li>Environment for productive work and career enhancement</li></br>
+          <li>Enhance the institutional image</li></br>
+          <li>To empower society through ICT</li></br>
+          <li>To make the country a knowledge hub</li>
+        </ol>
+        
+      </div>
+      <div class="holder">
+        <h2 class="title">INFRASTRUCTURE AND SERVICES</h2>
+        <div class="imgholder"><a href="infrastructure_and_services.html"><img src="../images/demo/library.png" alt="" /></a></div>
+        <div class="imgholder"><a href="infrastructure_and_services.html"><img src="../images/demo/lecture.png" alt="" /></a></div>
+        <div class="imgholder last"><a href="infrastructure_and_services.html"><img src="../images/demo/labs.png" alt="" /></a></div>
+      </div>
+    </div>
+    <!-- ####################################################################################################### --> 
+  </div>
+</div>
+<!-- ####################################################################################################### -->
+<div class="wrapper">
+  <div id="academiclinks" class="clear">
+    <div class="linkbox">
+      <h2>Degrees</h2>
+      <ul>
+        <li><a href="undergraduate_studies.html">&raquo; Undergraduate Progammes</a></li>
+        <li><a href="postgraduate_studies.html">&raquo; Postgraduate progammes</a></li>
+        <li><a href="#">&raquo; Research Degrees</a></li>
+        <li><a href="BIT.html">&raquo; External Degrees</a></li>
+        <li><a href="#">&raquo; Online Programmes</a></li>
+      </ul>
+    </div>
+    <div class="linkbox">
+      <h2>Mannagement</h2>
+      <ul>
+        <li><a href="#">&raquo; Academic & Publication</a></li>
+        <li><a href="#">&raquo; Establishments & Administratoin </a></li>
+        <li><a href="#">&raquo; Examination and Registraton</a></li>
+        <li><a href="#">&raquo; Engineering Division</a></li>
+        <li><a href="#">&raquo; Finance Division</a></li>
+        <li><a href="#">&raquo; Information Systems(NOC)</a></li>
+      </ul>
+    </div>
+    <div class="linkbox">
+      <h2>UCSC Centres</h2>
+      <ul>
+        <li><a href="centres.html">&raquo; Advace Digital Media Technologies Centre(ADMTC)</a></li>
+        <li><a href="centres1.html">&raquo; Computing Service Centre(CSC)</a></li>
+        <li><a href="centres1.html">&raquo; Digital Forencis Centre(DFC)</a></li>
+        <li><a href="centres.html">&raquo; E-Learning Centre(ELC)</a></li>
+        <li><a href="centres1.html">&raquo; External Degree Centre(EDC)</a></li>
+        <li><a href="centres.html">&raquo; Professional Development Centre(PDC)</a></li>
+      </ul>
+    </div>
+    <div class="linkbox last">
+      <h2>Departments</h2>
+      <ul>
+        <li><a href="#">&raquo; Computing and Intelligetn Systems</a></li>
+        <li><a href="#">&raquo; Infrmation systems Engineering</a></li>
+        <li><a href="#">&raquo; Comunicaton and Media Technologies</a></li>
+      </ul>
+    </div>
+  </div>
+</div>
+<!-- ####################################################################################################### -->
+<div class="wrapper">
+  <div id="footer" class="clear"> 
+    <!-- ####################################################################################################### -->
+    <div class="fl_left clear">
+      <div class="fl_left center"><img src="../images/demo/google.png" alt="" /><br />
+        <a href="#">Find Us With Google Maps &raquo;</a></div>
+      <address>
+      University of Colombo School of Computing<br />
+      UCSC Building Complex<br />
+      35,Reid Avenue,Colombo 07<br />
+      SRI LANKA<br />
+      <br />
+      Tel: +94 -11- 2581245/ 7<br />
+      Email: <a href="#">info@ucsc.cmb.ac.lk</a>
+      </address>
+    </div>
+    <div class="fl_right">
+      <div id="social" class="clear">
+        <p>Stay Up to Date With Whats Happening</p>
+        <ul>
+          <li><a style="background-position:0 0;" href="#">Twitter</a></li>
+          <li><a style="background-position:-72px 0;" href="#">LinkedIn</a></li>
+          <li><a style="background-position:-142px 0;" href="#">Facebook</a></li>
+          <li><a style="background-position:-212px 0;" href="#">Flickr</a></li>
+          <li><a style="background-position:-282px 0;" href="#">RSS</a></li>
+        </ul>
+      </div>
+      <div id="newsletter">
+        <form action="#" method="post">
+          <fieldset>
+            <legend>Subscribe To Our Newsletter:</legend>
+            <input type="text" value="Enter Email Here&hellip;" onfocus="this.value=(this.value=='Enter Email Here&hellip;')? '' : this.value ;" />
+            <input type="text" id="subscribe" value="Submit" />
+          </fieldset>
+        </form>
+      </div>
+    </div>
+    <div id="copyright" class="clear">
+      <p class="fl_left">Copyright &copy; 2016 - All Rights Reserved - <a href="#">University of Colombo School of Computing</a></p>
+    </div>
+    <!-- ####################################################################################################### --> 
+  </div>
+</div>
+</body>
+</html>
